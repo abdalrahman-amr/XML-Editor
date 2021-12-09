3f93c2edb1f7f16928d9a953c27b0eef086d060a
abdalrahman-amr <35754034+abdalrahman-amr@users.noreply.github.com>
2021-12-09 15:25:57 +0200

    Create formatting.cpp

---
+++ formatting.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+#include <fstream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+vector <string> xml_vector(string xml) {
+	stack <string> s;
+	ifstream file(xml);
+	string line;
+	vector <string> temp;
+	int i = 0;
+
+
+	for (; getline(file, line); i++) {
+		bool whiteSpacesOnly = std::all_of(line.begin(), line.end(), isspace);
+		line.erase(std::remove(line.begin(), line.end(), '\t'), line.end());
+		if (whiteSpacesOnly)temp.push_back(line + " ");
+		else temp.push_back(line);
+
+	}
+	return temp;
+
+}
+string formatting(string xml) {
+
+	vector <string> x;
+	string line;
+	x = xml_vector(xml);
+	string formatted;
+	int space_count = 0;
+	for (int i = 0; i < x.size(); i++) {
+		line = x[i];
+		if (line[0] == '<') {
+			if (line.find_first_of('<') != line.find_last_of('<')) {
+				for (int j = 0; j < space_count; j++) {
+					formatted.append("\t");
+				}
+				formatted.append(line);
+				formatted.append("\n");
+			}
+			else if (line[1] == '/') {
+				space_count--;
+				for (int j = 0; j < space_count; j++) {
+					formatted.append("\t");
+				}
+				formatted.append(line);
+				formatted.append("\n");
+
+			}
+			else {
+				for (int j = 0; j < space_count; j++) {
+					formatted.append("\t");
+				}
+				formatted.append(line);
+				formatted.append("\n");
+				space_count++;
+			}
+		}
+		else {
+			for (int j = 0; j < space_count; j++) {
+				formatted.append("\t");
+			}
+			formatted.append(line);
+			formatted.append("\n");
+
+		}
+	}
+	return formatted;
+
+}
+int main()
+{
+	string s;
+	s = formatting("sample.xml");
+	cout << s;
+
+}	
